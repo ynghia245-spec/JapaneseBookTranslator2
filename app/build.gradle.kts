f04294ebe5eeef4f3d06a7ea4de544e451a43090
ynghia245-spec <ynghia245@gmail.com>
2026-05-14 09:48:39 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.nghia.jptranslator"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
